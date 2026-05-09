9aca251feaeaad9950e11b5a6b4de7f9041acd77
Rahat Ahmed <87570918+Rahat0764@users.noreply.github.com>
2026-05-10 00:30:03 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  // needed for @mediapipe
+  transpilePackages: [
+    '@mediapipe/face_mesh',
+    '@mediapipe/camera_utils',
+    '@mediapipe/drawing_utils',
+    'face-api.js',
+    'groq-sdk',
+  ],
   webpack: (config) => {
     config.resolve.fallback = { fs: false, path: false };
     return config;
